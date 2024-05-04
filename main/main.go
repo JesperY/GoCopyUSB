e3fb0b107ae4ae8b34574c596788ee74559005a3
wangjie <JerryWon1214@gmail.com>
2024-05-04 14:26:24 +0800

    修改defer使用逻辑

--- main/main.go
+++ main/main.go
@@ -48,42 +48,7 @@ func main() {
 
 	fmt.Println("Listening for USB drive insertion events...")
 	for {
-		eventRaw, err := oleutil.CallMethod(result, "NextEvent", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		event := eventRaw.ToIDispatch()
-		defer event.Release()
-
-		targetInstance := oleutil.MustGetProperty(event, "TargetInstance")
-		instance := targetInstance.ToIDispatch()
-		defer instance.Release()
-
-		deviceId := oleutil.MustGetProperty(instance, "DeviceID").ToString()
-		fmt.Printf("USB Drive inserted: %s\n", deviceId)
-
-		sourcePath := deviceId + `\` // Assume the USB is mounted with a drive letter.
-		//fmt.Println(sourcePath)
-		targetPath := `D:\TargetDirectory\`
-
-		// Copy all files and directories from USB drive to target directory
-		err = filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			targetFilePath := filepath.Join(targetPath, strings.TrimPrefix(path, sourcePath))
-			if info.IsDir() {
-				return os.MkdirAll(targetFilePath, info.Mode())
-			} else {
-				return copyFile(path, targetFilePath)
-			}
-		})
-
-		if err != nil {
-			log.Println("Error copying files:", err)
-		} else {
-			fmt.Println("All files copied successfully from", deviceId)
-		}
+		handleEvent(result)
 	}
 }
 
@@ -93,14 +58,63 @@ func copyFile(src, dst string) error {
 	if err != nil {
 		return err
 	}
-	defer sourceFile.Close()
+	defer func(sourceFile *os.File) {
+		err := sourceFile.Close()
+		if err != nil {
+
+		}
+	}(sourceFile)
 
 	destinationFile, err := os.Create(dst)
 	if err != nil {
 		return err
 	}
-	defer destinationFile.Close()
+	defer func(destinationFile *os.File) {
+		err := destinationFile.Close()
+		if err != nil {
+
+		}
+	}(destinationFile)
 
 	_, err = io.Copy(destinationFile, sourceFile)
 	return err
+}
+
+// handleEvent 插入U盘时的处理逻辑
+func handleEvent(result *ole.IDispatch) {
+	eventRaw, err := oleutil.CallMethod(result, "NextEvent", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	event := eventRaw.ToIDispatch()
+	defer event.Release()
+
+	targetInstance := oleutil.MustGetProperty(event, "TargetInstance")
+	instance := targetInstance.ToIDispatch()
+	defer instance.Release()
+
+	deviceId := oleutil.MustGetProperty(instance, "DeviceID").ToString()
+	fmt.Printf("USB Drive inserted: %s\n", deviceId)
+
+	sourcePath := deviceId + `\` // Assume the USB is mounted with a drive letter.
+	targetPath := `D:\TargetDirectory\`
+
+	// Copy all files and directories from USB drive to target directory
+	err = filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		targetFilePath := filepath.Join(targetPath, strings.TrimPrefix(path, sourcePath))
+		if info.IsDir() {
+			return os.MkdirAll(targetFilePath, info.Mode())
+		} else {
+			return copyFile(path, targetFilePath)
+		}
+	})
+
+	if err != nil {
+		log.Println("Error copying files:", err)
+	} else {
+		fmt.Println("All files copied successfully from", deviceId)
+	}
 }
