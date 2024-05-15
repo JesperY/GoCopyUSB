cc08892c4719958d7a3827212b754536cf965b36
wangjie <JerryWon1214@gmail.com>
2024-05-16 00:56:46 +0800

    add some option to choose

--- frontend/pages/settings/settings.go
+++ frontend/pages/settings/settings.go
@@ -1,10 +1,12 @@
 package settings
 
 import (
+	"fmt"
 	"gioui.org/app"
 	"gioui.org/example/component/icon"
 	"gioui.org/layout"
-	"gioui.org/op/paint"
+	"gioui.org/op"
+	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
@@ -16,18 +18,28 @@ import (
 	"github.com/ncruces/zenity"
 	"image/color"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Page struct {
-	chooseDirBtn widget.Clickable
-	chooseBlkBtn widget.Clickable
-	clearBlkBtn  widget.Clickable
-	setDefault   widget.Bool
+	chooseDirBtn   widget.Clickable
+	chooseBlkBtn   widget.Clickable
+	clearBlkBtn    widget.Clickable
+	submitSufBtn   widget.Clickable
+	submitDelayBtn widget.Clickable
 	app.Window
 	*page.Router
 	widget.List
-	textList widget.List
-	text     []string
+	textList        widget.List
+	text            []string
+	blkSuffix       component.TextField
+	delayTime       component.TextField
+	inputAlignment  text.Alignment
+	inputAlignment2 text.Alignment
+	dialogOpen      bool
+	closeDialogBtn  widget.Clickable
 }
 
 func (p Page) Actions() []component.AppBarAction {
@@ -40,6 +52,7 @@ func (p Page) Overflow() []component.OverflowAction {
 
 func (p *Page) Layout(gtx layout.Context, th *material.Theme) layout.Dimensions {
 	p.List.Axis = layout.Vertical
+
 	return material.List(th, &p.List).Layout(gtx, 1, func(gtx layout.Context, _ int) layout.Dimensions {
 		return layout.Flex{
 			Alignment: layout.Middle,
@@ -110,6 +123,7 @@ func (p *Page) Layout(gtx layout.Context, th *material.Theme) layout.Dimensions 
 											for _, folder := range folders {
 												config.ConfigPtr.WhiteListDir = append(config.ConfigPtr.WhiteListDir, folder)
 											}
+											config.ConfigPtr.WriteConfig()
 											p.Window.Invalidate()
 										}
 										return material.Button(th, &p.chooseBlkBtn, "点击选择黑名单").Layout(gtx)
@@ -142,6 +156,8 @@ func (p *Page) Layout(gtx layout.Context, th *material.Theme) layout.Dimensions 
 										gtx.Constraints.Max.X = gtx.Dp(buttonWidth)
 										gtx.Constraints.Max.Y = gtx.Dp(buttonHeight)
 										if p.clearBlkBtn.Clicked(gtx) {
+											config.ConfigPtr.WhiteListDir = nil
+											config.ConfigPtr.WriteConfig()
 											p.Window.Invalidate()
 										}
 										return material.Button(th, &p.clearBlkBtn, "清空黑名单").Layout(gtx)
@@ -150,150 +166,130 @@ func (p *Page) Layout(gtx layout.Context, th *material.Theme) layout.Dimensions 
 							}),
 						)
 					}),
+					// todo 把下面的文本框往右边移一点
 					// 在右侧添加一个带边框且固定大小的可滚动文本框
 					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-						// 设置文本框的大小
-						textboxWidth := gtx.Dp(unit.Dp(400))
-						textboxHeight := gtx.Dp(unit.Dp(150))
-						gtx.Constraints.Min.X = textboxWidth
-						gtx.Constraints.Min.Y = textboxHeight
-						gtx.Constraints.Max.X = textboxWidth
-						gtx.Constraints.Max.Y = textboxHeight
 
 						border := widget.Border{
 							Color:        color.NRGBA{R: 0, G: 0, B: 0, A: 255},
 							CornerRadius: unit.Dp(4),
 							Width:        unit.Dp(2),
 						}
-						return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-							// 使用 layout.Stack 来确保内容和滚动条正确布局
-							return layout.Stack{}.Layout(gtx, layout.Expanded(func(gtx layout.Context) layout.Dimensions {
-								p.textList.Axis = layout.Vertical
-								return material.List(th, &p.textList).Layout(gtx, len(config.ConfigPtr.WhiteListDir), func(gtx layout.Context, index int) layout.Dimensions {
-									// 设置背景颜色
-									bgColor := color.NRGBA{R: 240, G: 240, B: 240, A: 255} // 默认颜色
-									if index%2 == 0 {
-										bgColor = color.NRGBA{R: 220, G: 220, B: 220, A: 255} // 另一种颜色
-									}
-
-									return layout.Stack{}.Layout(gtx,
-										layout.Expanded(func(gtx layout.Context) layout.Dimensions {
-											paint.Fill(gtx.Ops, bgColor)
-											gtx.Constraints.Min.Y = 30
-											gtx.Constraints.Max.X = 400
-											return layout.Dimensions{Size: gtx.Constraints.Min}
-										}),
-										layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-											// 设置行高
-											gtx.Constraints.Min.Y = gtx.Dp(unit.Dp(30))
-											gtx.Constraints.Max.Y = gtx.Dp(unit.Dp(30))
-											gtx.Constraints.Min.X = gtx.Dp(unit.Dp(400))
-											gtx.Constraints.Max.X = gtx.Dp(unit.Dp(400))
-											insets := layout.UniformInset(unit.Dp(0))
-											return insets.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-												return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-													return material.Body1(th, config.ConfigPtr.WhiteListDir[index]).Layout(gtx)
+						return layout.Inset{Left: unit.Dp(75), Right: unit.Dp(8)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								// 使用 layout.Stack 来确保内容和滚动条正确布局
+								return layout.Stack{}.Layout(gtx, layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+									p.textList.Axis = layout.Vertical
+									// 设置文本框的大小
+									textboxWidth := gtx.Dp(unit.Dp(350))
+									textboxHeight := gtx.Dp(unit.Dp(150))
+									gtx.Constraints.Min.X = textboxWidth
+									gtx.Constraints.Min.Y = textboxHeight
+									gtx.Constraints.Max.X = textboxWidth
+									gtx.Constraints.Max.Y = textboxHeight
+									return material.List(th, &p.textList).Layout(gtx, len(config.ConfigPtr.WhiteListDir), func(gtx layout.Context, index int) layout.Dimensions {
+										return layout.Stack{}.Layout(gtx,
+											layout.Stacked(func(gtx layout.Context) layout.Dimensions {
+												// 设置行高
+												gtx.Constraints.Min.Y = gtx.Dp(unit.Dp(30))
+												gtx.Constraints.Max.Y = gtx.Dp(unit.Dp(30))
+												gtx.Constraints.Min.X = gtx.Dp(unit.Dp(400))
+												gtx.Constraints.Max.X = gtx.Dp(unit.Dp(400))
+												insets := layout.UniformInset(unit.Dp(0))
+												return insets.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+													return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+														return material.Body1(th, config.ConfigPtr.WhiteListDir[index]).Layout(gtx)
+													})
 												})
-											})
-										}),
-									)
-								})
-							}))
+											}),
+										)
+									})
+								}))
+							})
 						})
 					}),
 				)
 			}),
-			//// 黑名单
-			//layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			//	// 固定按钮宽度和高度
-			//	buttonWidth := unit.Dp(150)
-			//	buttonHeight := unit.Dp(50)
-			//	// 外边距
-			//	inset := layout.Inset{
-			//		Left: unit.Dp(10),
-			//	}
-			//	return inset.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-			//		return layout.Stack{}.Layout(gtx, layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-			//			// 设置按钮的最小宽度和高度
-			//			gtx.Constraints.Min.X = gtx.Dp(buttonWidth)
-			//			gtx.Constraints.Min.Y = gtx.Dp(buttonHeight)
-			//			// 设置按钮的最大宽度和高度（可选）
-			//			gtx.Constraints.Max.X = gtx.Dp(buttonWidth)
-			//			gtx.Constraints.Max.Y = gtx.Dp(buttonHeight)
-			//			if p.chooseDirBtn.Clicked(gtx) {
-			//				path, _, err := dlgs.File("Select Folder", "", true)
-			//				if err != nil {
-			//					log.Println("Error selecting folder:", err)
-			//				} else {
-			//					if path == "" {
-			//						_, err := dlgs.Info("路径不可用", "请选择一个有效的文件夹路径。")
-			//						if err != nil {
-			//							log.Println("Error displaying dialog:", err)
-			//						}
-			//					}
-			//					config.ConfigPtr.TargetDir = path
-			//					config.ConfigPtr.WriteConfig()
-			//					p.Window.Invalidate()
-			//				}
-			//			}
-			//			return material.Button(th, &p.chooseBlkBtn, "点击选择黑名单").Layout(gtx)
-			//		}))
-			//
-			//	})
-			//}),
-			//// 在两个按钮之间添加空格
-			//layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			//	spacerWidth := unit.Dp(20)
-			//	return layout.Spacer{Width: spacerWidth, Height: unit.Dp(10)}.Layout(gtx)
-			//}),
-			//layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			//	// 固定按钮宽度和高度
-			//	buttonWidth := unit.Dp(150)
-			//	buttonHeight := unit.Dp(50)
-			//	// 外边距
-			//	inset := layout.Inset{
-			//		Left: unit.Dp(10),
-			//	}
-			//	return inset.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-			//		return layout.Stack{}.Layout(gtx, layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-			//			// 设置按钮的最小宽度和高度
-			//			gtx.Constraints.Min.X = gtx.Dp(buttonWidth)
-			//			gtx.Constraints.Min.Y = gtx.Dp(buttonHeight)
-			//			// 设置按钮的最大宽度和高度（可选）
-			//			gtx.Constraints.Max.X = gtx.Dp(buttonWidth)
-			//			gtx.Constraints.Max.Y = gtx.Dp(buttonHeight)
-			//			if p.chooseDirBtn.Clicked(gtx) {
-			//				path, _, err := dlgs.File("Select Folder", "", true)
-			//				if err != nil {
-			//					log.Println("Error selecting folder:", err)
-			//				} else {
-			//					if path == "" {
-			//						_, err := dlgs.Info("路径不可用", "请选择一个有效的文件夹路径。")
-			//						if err != nil {
-			//							log.Println("Error displaying dialog:", err)
-			//						}
-			//					}
-			//					config.ConfigPtr.TargetDir = path
-			//					config.ConfigPtr.WriteConfig()
-			//					p.Window.Invalidate()
-			//				}
-			//			}
-			//			return material.Button(th, &p.chooseBlkBtn, "点击选择黑名单").Layout(gtx)
-			//		}))
-			//	})
-			//}),
-			////TODO  在下方添加代码，绘制一个右侧的文本框
-			//// 在右侧添加一个可滚动的文本框
-			//layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-			//	return layout.Stack{}.Layout(gtx, layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-			//		// 定义文本框内容
-			//		text := []string{"Line 1", "Line 2", "Line 3", "Line 4", "Line 5", "Line 6", "Line 7", "Line 8"}
-			//		list := layout.List{Axis: layout.Vertical}
-			//		return list.Layout(gtx, len(text), func(gtx layout.Context, index int) layout.Dimensions {
-			//			return material.Body1(th, text[index]).Layout(gtx)
-			//		})
-			//	}))
-			//}),
+			// 添加一些空格
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				spacerWidth := unit.Dp(20)
+				return layout.Spacer{Width: spacerWidth, Height: unit.Dp(10)}.Layout(gtx)
+			}),
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				return layout.Flex{
+					Axis: layout.Horizontal,
+				}.Layout(gtx,
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return layout.Inset{Right: unit.Dp(8)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return layout.Dimensions{Size: gtx.Constraints.Min}
+						})
+					}),
+					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+						p.blkSuffix.Alignment = p.inputAlignment
+						return p.blkSuffix.Layout(gtx, th, `请在此输入您不想备份的文件后缀名，以空格隔开`)
+					}),
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						if p.submitSufBtn.Clicked(gtx) {
+							str := p.blkSuffix.Text()
+							split := strings.Split(str, " ")
+							for _, s := range split {
+								config.ConfigPtr.WhiteListSuffix = append(config.ConfigPtr.WhiteListSuffix, s)
+							}
+							config.ConfigPtr.WriteConfig()
+						}
+						// 设置按钮的最小和最大宽度
+						btn := material.Button(th, &p.submitSufBtn, "确认")
+						//btnSize := layout.Dimensions{Size: gtx.Constraints.Min}
+						return layout.Inset{Left: unit.Dp(8), Right: unit.Dp(8), Top: unit.Dp(12)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							gtx.Constraints.Min.X = gtx.Dp(80)
+							gtx.Constraints.Max.X = gtx.Dp(80)
+							return btn.Layout(gtx)
+						})
+					}),
+				)
+			}),
+			// 添加一些空格
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				spacerWidth := unit.Dp(20)
+				return layout.Spacer{Width: spacerWidth, Height: unit.Dp(10)}.Layout(gtx)
+			}),
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				return layout.Flex{
+					Axis: layout.Horizontal,
+				}.Layout(gtx,
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return layout.Inset{Right: unit.Dp(8)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return layout.Dimensions{Size: gtx.Constraints.Min}
+						})
+					}),
+					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+						p.delayTime.Alignment = p.inputAlignment2
+						return p.delayTime.Layout(gtx, th, `请在此输入要延迟备份的分钟数(请输入整数)`)
+					}),
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						if p.submitDelayBtn.Clicked(gtx) {
+							str := p.delayTime.Text()
+							minute, err := stringToInt(str)
+							if err != nil {
+								go p.showErrorWindow(th, "输入错误", "请输入一个有效的整数")
+							} else {
+								config.ConfigPtr.DelayMinutes = minute
+								config.ConfigPtr.WriteConfig()
+								fmt.Println(minute)
+							}
+							config.ConfigPtr.WriteConfig()
+						}
+						// 设置按钮的最小和最大宽度
+						btn := material.Button(th, &p.submitDelayBtn, "确认")
+						//btnSize := layout.Dimensions{Size: gtx.Constraints.Min}
+						return layout.Inset{Left: unit.Dp(8), Right: unit.Dp(8), Top: unit.Dp(12)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							gtx.Constraints.Min.X = gtx.Dp(80)
+							gtx.Constraints.Max.X = gtx.Dp(80)
+							return btn.Layout(gtx)
+						})
+					}),
+				)
+			}),
 		)
 	})
 }
@@ -309,4 +305,49 @@ func New(router *page.Router) *Page {
 	return &Page{
 		Router: router,
 	}
+}
+func stringToInt(s string) (int, error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+// 弹窗提示
+func (p *Page) showErrorWindow(th *material.Theme, title, message string) {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title(title),
+			app.Size(unit.Dp(300), unit.Dp(200)))
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				return
+			case app.FrameEvent:
+				gtx := app.NewContext(&op.Ops{}, e)
+				layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Axis: layout.Vertical,
+					}.Layout(gtx,
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							return material.H6(th, title).Layout(gtx)
+						}),
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							return material.Body1(th, message).Layout(gtx)
+						}),
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							var closeBtn widget.Clickable
+							btn := material.Button(th, &closeBtn, "关闭")
+							if closeBtn.Clicked(gtx) {
+								os.Exit(0)
+							}
+							return layout.Inset{Top: unit.Dp(8)}.Layout(gtx, btn.Layout)
+						}),
+					)
+				})
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
 }
--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"gioui.org/app"
 	"gioui.org/font/gofont"
-	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/text"
-	"gioui.org/unit"
 	"gioui.org/widget/material"
 	"github.com/JesperY/GoCopyUSB/backend"
-	"github.com/JesperY/GoCopyUSB/config"
 	"github.com/JesperY/GoCopyUSB/copylogger"
 	page "github.com/JesperY/GoCopyUSB/frontend/pages"
 	"github.com/JesperY/GoCopyUSB/frontend/pages/settings"
-	"image/color"
 	"os"
 )
 
@@ -51,49 +46,4 @@ func loop(window *app.Window) error {
 		}
 	}
 
-}
-
-// SetWindowOptions
-//
-//	@Description: 设置窗口的各项属性
-//	@param title 标题文字
-//	@param width 窗口长度 单位DP 下同
-//	@param height 窗口宽度
-func SetWindowOptions(window *app.Window, title string, width, height int) {
-	window.Option(app.Title(title), app.Size(unit.Dp(width), unit.Dp(height)))
-}
-
-// ListenEvent
-//
-//	@Description: 监听页面内容
-//	@param window 窗口对象指针
-func ListenEvent(window *app.Window) {
-	theme := material.NewTheme()
-	var ops op.Ops
-	for {
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			//os.Exit(0)
-			//todo 点击关闭时程序缩小至托盘
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-			// Define a large label with an appropriate text:
-			title := material.H6(theme, "USB备份监听已启动")
-			dirText := material.Body1(theme, fmt.Sprintf(`当前备份目标目录为%s`, config.ConfigPtr.TargetDir))
-
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			dirColor := color.NRGBA{R: 127, G: 55, B: 123, A: 255}
-			dirText.Color = dirColor
-			// Change the position of the label.
-			title.Alignment = text.Middle
-			dirText.Alignment = text.Middle
-			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-				layout.Rigid(title.Layout),
-				layout.Rigid(dirText.Layout),
-			)
-			e.Frame(gtx.Ops)
-		}
-	}
 }
